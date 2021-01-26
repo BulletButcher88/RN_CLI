27aeb3beda2f491c574b97576f14a00293329ebe
Mark Butcher <markjamesbutcher@gmail.com>
2021-01-26 21:13:59 +1100

    first commit

---
+++ App.js
@@ -0,0 +1,71 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
+
+
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Button
+} from 'react-native';
+
+
+const App: () => React$Node = () => {
+
+  const pickImageHandler = () => {
+
+    var options = {
+      title: 'Select Image',
+      customButtons: [
+        {
+          name: 'customOptionKey',
+          title: 'Choose Photo from Custom Option'
+        },
+      ],
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    launchCamera(options, response => {
+
+      console.log('Response = ', response);
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log(
+          'User tapped custom button: ',
+          response.customButton
+        );
+        alert(response.customButton);
+      } else {
+        console.log(response);
+      }
+    });
+  }
+
+  return (
+    <View style={styles.screen}>
+      <Button title="Take an image" onPress={pickImageHandler} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+});
+
+export default App;
